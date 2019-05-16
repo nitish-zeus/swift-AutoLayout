aa380e2a512e52cb13308764582c48aec8fc3587
Nitish Kumar <nitish.kumar@tarento.com>
2019-05-16 19:30:20 +0530

    Added currency icon

--- SwiftAutolyout/SwiftAutolyout/Views/CurrencyTextField.swift
+++ SwiftAutolyout/SwiftAutolyout/Views/CurrencyTextField.swift
@@ -14,6 +14,23 @@ class CurrencyTextField: UITextField {
         customiseView()
         
     }
+    override func draw(_ rect: CGRect) {
+        
+        let size:CGFloat = 20
+        let currencyLb = UILabel(frame: CGRect(x: 5, y: (frame.size.height/2) - size/2, width: size, height: size))
+        currencyLb.backgroundColor = #colorLiteral(red: 0.7797960639, green: 0.7799281478, blue: 0.7797787189, alpha: 1)
+        currencyLb.textAlignment = .center
+        currencyLb.textColor = #colorLiteral(red: 0.4999483824, green: 0.50003618, blue: 0.4999368191, alpha: 1)
+        currencyLb.layer.cornerRadius = 5.0
+        currencyLb.clipsToBounds = true
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = .current
+        currencyLb.text = numberFormatter.currencySymbol
+        addSubview(currencyLb)
+        
+        
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -25,6 +42,7 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         if placeholder == nil {
             placeholder = " "
         }
